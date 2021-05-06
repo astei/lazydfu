310e70bd76a4c287dc29bf4e183ce0b8f96a0920
Tofik <slodkitofik@gmail.com>
2021-05-06 13:07:50 +0200

    Ported to Mojang Mappings

--- src/main/java/me/steinborn/lazydfu/mixin/DataFixersMixin.java
+++ src/main/java/me/steinborn/lazydfu/mixin/DataFixersMixin.java
@@ -3,8 +3,8 @@ package me.steinborn.lazydfu.mixin;
 import com.mojang.datafixers.DataFixer;
 import com.mojang.datafixers.DataFixerBuilder;
 import me.steinborn.lazydfu.mod.LazyDataFixerBuilder;
-import net.minecraft.datafixer.Schemas;
-import net.minecraft.util.Util;
+import net.minecraft.util.datafix.DataFixers;
+import net.minecraft.Util;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -15,22 +15,22 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.TimeUnit;
 
-@Mixin(Schemas.class)
-public class SchemasMixin {
+@Mixin(DataFixers.class)
+public class DataFixersMixin {
 //    private static long startTime;
 //
-//    @Inject(method = "create", at = @At("HEAD"))
-//    private static void create$recordStart(CallbackInfoReturnable<DataFixer> ci) {
+//    @Inject(method = "createFixerUpper", at = @At("HEAD"))
+//    private static void createFixerUpper$recordStart(CallbackInfoReturnable<DataFixer> ci) {
 //        startTime = System.nanoTime();
 //    }
 
-    @Redirect(method = "create", at = @At(value = "NEW", target = "com/mojang/datafixers/DataFixerBuilder"))
-    private static DataFixerBuilder create$replaceBuilder(int dataVersion) {
+    @Redirect(method = "createFixerUpper", at = @At(value = "NEW", target = "com/mojang/datafixers/DataFixerBuilder"))
+    private static DataFixerBuilder createFixerUpper$replaceBuilder(int dataVersion) {
         return new LazyDataFixerBuilder(dataVersion);
     }
 
-//    @Inject(method = "create", at = @At("RETURN"))
-//    private static void create$doEnd(CallbackInfoReturnable<DataFixer> ci) {
+//    @Inject(method = "createFixerUpper", at = @At("RETURN"))
+//    private static void createFixerUpper$doEnd(CallbackInfoReturnable<DataFixer> ci) {
 //        new Thread(() -> {
 //            if (((ForkJoinPool)Util.getBootstrapExecutor()).awaitQuiescence(Integer.MAX_VALUE, TimeUnit.MILLISECONDS)) {
 //                System.out.println("Initialization complete in " + ((System.nanoTime() - startTime) / 1_000_000) + "ms");
