e63d33542370612e7de4384712d19b80f2e85696
Corgi_Taco <66983020+CorgiTaco@users.noreply.github.com>
2021-11-30 14:56:45 -0800

    Update to 1.18

--- src/main/java/me/steinborn/lazydfu/LazyDFU.java
+++ src/main/java/me/steinborn/lazydfu/LazyDFU.java
@@ -1,7 +1,11 @@
 package me.steinborn.lazydfu;
 
 import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @Mod("lazydfu")
 public class LazyDFU {
+
+    public static final Logger LOGGER = LogManager.getLogger();
 }
