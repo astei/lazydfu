3f13b8f957ae3dba1a88dda04948cedc7024b203
Corgi Taco <66983020+CorgiTaco@users.noreply.github.com>
2023-01-11 00:44:17 -0800

    Remove unnecessary logger

--- src/main/java/me/steinborn/lazydfu/LazyDFU.java
+++ src/main/java/me/steinborn/lazydfu/LazyDFU.java
@@ -5,7 +5,4 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 @Mod("lazydfu")
-public class LazyDFU {
-
-    public static final Logger LOGGER = LogManager.getLogger();
-}
+public class LazyDFU {}
