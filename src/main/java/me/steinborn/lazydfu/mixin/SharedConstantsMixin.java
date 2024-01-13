55f77e58258d287773c48cc4161d9edecdefee83
Rhys <98863820+rhysdh540@users.noreply.github.com>
2024-01-13 16:22:06 -0500

    replace overwrite with inject

--- src/main/java/me/steinborn/lazydfu/mixin/SharedConstantsMixin.java
+++ src/main/java/me/steinborn/lazydfu/mixin/SharedConstantsMixin.java
@@ -2,16 +2,18 @@ package me.steinborn.lazydfu.mixin;
 
 import net.minecraft.SharedConstants;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(SharedConstants.class)
+@Mixin(value = SharedConstants.class, priority = Integer.MAX_VALUE)
 public class SharedConstantsMixin {
     /**
      * @author Andrew Steinborn
      * @reason Disables any possibility of enabling DFU "optimizations"
      */
-    @Overwrite
-    public static void enableDataFixerOptimizations() {
-        // Turn this method into a no-op.
+    @Inject(method = "enableDataFixerOptimizations", at = @At("HEAD"), cancellable = true)
+    private static void disableDataFixerOptimizations(CallbackInfo ci) {
+        ci.cancel();
     }
 }
